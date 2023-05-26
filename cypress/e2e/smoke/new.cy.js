c198faab17e68a1a1c632e2260231586432581c4
venkatkotapati <57140203+venkatkotapati@users.noreply.github.com>
2023-05-26 17:01:22 +0530

    test new changes

--- cypress/e2e/smoke/new.cy.js
+++ cypress/e2e/smoke/new.cy.js
@@ -5,7 +5,6 @@ import Constants from '../../support/Locators/Locators';
 import  { login } from '../../support/Help/utilit';
 //import { UtilityClass } from '../../support/Help/utility';
 
-//let UtilityHelper = new UtilityClass();
 //var UtilityHelper1 = new UtilityClass();
 //Cypress.env('RETRIES', 1);
 describe('template spec', { retries: { openMode: 3, } }, () => {
